fdb84910aafab2accb102aa2ce00f7cba2d311b6
ashishgulatiG4G <ashishgulati2112@gmail.com>
2024-07-28 22:20:22 +0530

    Source code for Class - 9 Spring Boot intro + REST API's

---
+++ Class-9_springStarter/src/main/java/com/example/Class_9_springStarter/Class9SpringStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Class_9_springStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class9SpringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class9SpringStarterApplication.class, args);
+	}
+
+}
---
+++ Class-9_springStarter/src/main/java/com/example/Class_9_springStarter/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.example.Class_9_springStarter;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Base URL : Domain Name / IP_Address:PORT [127.0.0.1:8080]
+ * <p>
+ * Base URL/endpoint [API]
+ * <p>
+ * Steps to write an API:
+ *      1. Define an HTTP method [Request Method]
+ *      2. Define the end point / API path
+ *      3. Define how to take an input
+ */
+@RestController
+public class EmployeeController {
+
+
+
+}
