5c4550240079eb00a070b4b7e1770d9f75e00a7c
ashishgulatiG4G <ashishgulati2112@gmail.com>
2024-08-04 21:31:37 +0530

    Added source code for Class - 11 Rest Api with DB

---
+++ Class-11_DI_IOC/src/main/java/com/example/Class_10_DI_IOC/Class10DiIocApplication.java
@@ -0,0 +1,13 @@
+package com.example.Class_10_DI_IOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class10DiIocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class10DiIocApplication.class, args);
+	}
+
+}
---
+++ Class-11_DI_IOC/src/test/java/com/example/Class_10_DI_IOC/Class10DiIocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Class_10_DI_IOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class10DiIocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
