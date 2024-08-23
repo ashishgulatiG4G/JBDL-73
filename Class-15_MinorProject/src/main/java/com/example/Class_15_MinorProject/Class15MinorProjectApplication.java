951c7cf44e4b9f5f9716e8a72d7afa3f010544f6
ashishgulatiG4G <ashishgulati2112@gmail.com>
2024-08-23 20:17:52 +0530

    Added code for minor project

---
+++ Class-15_MinorProject/src/main/java/com/example/Class_15_MinorProject/Class15MinorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Class_15_MinorProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class15MinorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class15MinorProjectApplication.class, args);
+	}
+
+}
